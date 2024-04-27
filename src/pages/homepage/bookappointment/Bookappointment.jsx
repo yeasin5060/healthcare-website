d89bb43df841012ca5006e4f0494616840d1878a
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-04-27 19:43:43 +0600

    clinic_project_update_home_section_book_appointment_part_002

--- src/pages/homepage/bookappointment/Bookappointment.jsx
+++ src/pages/homepage/bookappointment/Bookappointment.jsx
@@ -12,9 +12,44 @@ const Bookappointment = () => {
                 <Subheading text="Book an Appointment" style="book_appointment_contant_head"/>
                 <Pera text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque tortor ornare ornare. Convallis felis vitae tortor augue. Velit nascetur proin massa in. Consequat faucibus porttitor enim et." style="book_appointment_contant_pera"/>
             </div>
-            <div className='book_appointment_form_box'>
-
-            </div>
+            <form className='book_appointment_form_box'>
+                <div className='book_appointment_form_container_box'>
+                    <div className='book_appointment_input_container'>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="text" placeholder='Name' />
+                        </div>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="email" placeholder='Email' />
+                        </div>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="date" placeholder='Date' />
+                        </div>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="text" placeholder='Doctor' />
+                        </div>
+                    </div>
+                    <div className='book_appointment_input_box'>
+                    <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="text" placeholder='Gender' />
+                        </div>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="email" placeholder='Phone' />
+                        </div>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="text" placeholder='Time' />
+                        </div>
+                        <div className='book_appointment_input_box'>
+                            <input className='book_appointment_inputs' type="text" placeholder='Department' />
+                        </div>
+                    </div>
+                </div>
+                <div className='book_appointment_textarea_box'>
+                    <textarea className='book_appointment_textarea' type="text" placeholder='Message' />
+                </div>
+                <div className='book_appointment_form_btn_box'>
+                    <button className='book_appointment_form_btn'>Submit</button>
+                </div>
+            </form>
         </div>
     </div>
    </section>
