2af98e673fd1cdb511563973ed6fab0f2d2efb55
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-05-06 21:50:17 +0600

    clinic_project_apdate_about_section_add_aboutbanertwo_part

--- src/pages/aboutpage/Aboutpage.jsx
+++ src/pages/aboutpage/Aboutpage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Aboutbaner from './aboutbaner/Aboutbaner'
 import Aboutservice from './aboutservice/Aboutservice'
+import Aboutbanertwo from './aboutbanertwo/Aboutbanertwo'
 
 const Aboutpage = () => {
   return (
     <>
     <Aboutbaner/>
     <Aboutservice/>
+    <Aboutbanertwo/>
     </>
   )
 }
---
+++ src/pages/aboutpage/aboutbanertwo/Aboutbanertwo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Pera from '../../../utilities/pera/Pera'
+import Nanoheading from '../../../utilities/nanohead/Nanoheading'
+import imageone from '../../../images/aboutimagetwo.png'
+import { FaQuoteLeft } from "react-icons/fa";
+import './Aboutbanertwo.css'
+
+const Aboutbanertwo = () => {
+  return (
+    <section id='aboutbanertwo'>
+        <img src={imageone} alt="not found" />
+        <div className='aboutbanertwo_contant_wrapper'>
+            <div className='aboutbanertwo_contant_box'>
+                <div className='aboutbanertwo_contant_image_box'>
+                    <FaQuoteLeft className='aboutbaner_tow_icon' />
+                </div>
+                <div className='aboutbanertwo_contant_box'>
+                    <Pera text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque tortor ornare ornare. Quisque placerat scelerisque felis vitae tortor augue. Velit nascetur Consequat faucibus porttitor enim et." style="aboutbanertwo_contant_pera"/>
+                    <Nanoheading text="John Doe" style="aboutbanertwo_contant_nanohead"/>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Aboutbanertwo
