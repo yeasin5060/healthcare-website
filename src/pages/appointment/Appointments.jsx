555ba987f0c21fdb9d3d72296949508adc8e9ea5
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-05-31 14:44:51 +0600

    clinic-website-update-appointment-page-add-appointment-location

--- src/pages/appointment/Appointments.jsx
+++ src/pages/appointment/Appointments.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Appointmentbaners from './appointmentbaner/Appointmentbaners'
+import Appointmentlocation from './appointmentlocation/Appointmentlocation'
 
 
 const Appointments = () => {
   return (
     <>
        <Appointmentbaners/>
+       <Appointmentlocation/>
     </>
   )
 }
