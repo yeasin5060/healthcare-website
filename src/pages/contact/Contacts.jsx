5a6685071f0137cc3a3ad3d6b65de44371a87b99
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-05-31 08:01:33 +0600

    clinic-website-update-contacts-page-add-contactlocaition-section

--- src/pages/contact/Contacts.jsx
+++ src/pages/contact/Contacts.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Contactbaner from './contactbaner/Contactbaner'
+import Contactlocation from './contactlocation/Contactlocation'
 
 const Contacts = () => {
   return (
     <>
         <Contactbaner/>
+        <Contactlocation/>
     </>
   )
 }
---
+++ src/pages/contact/contactlocation/Contactlocation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Contactlocation.css'
+
+const Contactlocation = () => {
+  return (
+    <section id='contact-location'>
+        <div className='container'>
+            <div className="contact-location-box">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.7523488014804!2d90.37409839999998!3d23.79183160000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c0cd0c2ee291%3A0x29fc2611200f2d6a!2sDental%20Art%20-%20Aesthetic%20Smile%20Clinic!5e0!3m2!1sen!2sbd!4v1717120604836!5m2!1sen!2sbd" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Contactlocation
